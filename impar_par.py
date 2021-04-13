80c334429e32df6a87e963aac3c3a1be13f8da3f
Thiago Tuissi Paião <81596921+ThiagoTuissiPaiao@users.noreply.github.com>
2021-04-12 23:06:58 -0300

    Update impar_par.py

--- impar_par.py
+++ impar_par.py
@@ -1,21 +1,6 @@
-#Crie um programa que leia um numero interio e mostre na tela se ela pe par ou impar
-
 numero = int(input("Digite um numero: "))
 
 if (numero % 2) == 0:
     print("O numero que você escolheu é PAR")
 else:
     print("O numero que você escolheu é IMPAR")
-
-"""
-#programação em ingles
-
-number = int(input("Write a number"))
-result = number % 2
-
-if result == 0:
-    print("The number is pair")
-else:
-    print("The number is odd")
-
-"""
